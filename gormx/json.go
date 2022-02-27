59c72d9f5f8e276cabea78ce201589c67b2f2377
sugar <qxsugar@gmail.com>
2022-02-27 21:42:53 +0800

    [fix] add json

---
+++ gormx/json.go
@@ -0,0 +1,38 @@
+package gormx
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"github.com/pkg/errors"
+)
+
+type JSON json.RawMessage
+
+func (j *JSON) MarshalJSON() ([]byte, error) {
+	return *j, nil
+}
+
+func (j *JSON) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+
+	result := json.RawMessage{}
+	err := json.Unmarshal(bytes, &result)
+	*j = JSON(result)
+	return err
+}
+
+func (j JSON) Value() (driver.Value, error) {
+	if len(j) == 0 {
+		return nil, nil
+	}
+	return json.RawMessage(j).MarshalJSON()
+}
+
+var _ driver.Valuer = (*JSON)(nil)
+var _ sql.Scanner = (*JSON)(nil)
+var _ json.Marshaler = (*JSON)(nil)
