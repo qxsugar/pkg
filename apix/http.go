826fdb98c9fcde58954f5d49cd429dd183cfaab7
sugar <qxsugar@gmail.com>
2022-06-14 16:43:01 +0800

    fix pong

--- apix/http.go
+++ apix/http.go
@@ -2,9 +2,8 @@ package apix
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
-func Pong(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "pong")
+func Pong(ctx *gin.Context) (interface{}, error) {
+	return "pong", nil
 }
