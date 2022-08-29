3e776b25d2183bffdb1c3a4337e0960fb4d5831b
sugar <qxsugar@gmail.com>
2022-08-29 19:02:43 +0800

    ref

--- apix/errors.go
+++ apix/errors.go
@@ -2,93 +2,123 @@ package apix
 
 import (
 	"go.uber.org/zap"
+	"net/http"
 )
 
-func (e RespBody) Error() string {
+type Exception struct {
+	HttpCode int
+	Code     int
+	Msg      string
+	Desc     string
+}
+
+func (e *Exception) GetHttpCode() int {
+	return e.HttpCode
+}
+
+func (e *Exception) GetCode() int {
+	return e.Code
+}
+
+func (e *Exception) GetMsg() string {
 	return e.Msg
 }
 
-const TAG = "[API_ERROR]"
+func (e *Exception) GetDesc() string {
+	return e.Desc
+}
 
-func newError(err error, code int, msg string) RespBody {
+func (e Exception) Error() string {
+	return e.Msg
+}
+
+var _ ApiException = (*Exception)(nil)
+
+func newException(httpCode, code int, msg string, err error) error {
 	logger := zap.S()
-	logger.Warnw(TAG, "err", err, "code", code, "msg", msg)
+	logger.Infof("%s: httpCode:%d, code: %d, msg: %s, err: %v", "[API_ERROR]", httpCode, code, msg, err)
 	if msg == "" {
 		msg = messages[code]
 	}
+
 	desc := ""
 	if err != nil {
 		desc = err.Error()
 	}
-	return RespBody{
-		Code:      code,
-		Msg:       msg,
-		Succeeded: false,
-		RespData:  nil,
-		Desc:      desc,
+
+	return &Exception{
+		HttpCode: httpCode,
+		Code:     code,
+		Msg:      msg,
+		Desc:     desc,
 	}
 }
 
+// NewCustomError 自定义消息
+func NewCustomError(code int, msg string) error {
+	return newException(http.StatusOK, code, msg, nil)
+}
+
 func NewInvalidArgumentError(err error, msg string) error {
-	return newError(err, InvalidArgument, msg)
+	return newException(InvalidArgument, InvalidArgument, msg, err)
 }
 
 func NewFailedPreconditionError(err error, msg string) error {
-	return newError(err, FailedPrecondition, msg)
+	return newException(FailedPrecondition, FailedPrecondition, msg, err)
 }
 
 func NewOutOfRangeError(err error, msg string) error {
-	return newError(err, OutOfRange, msg)
+	return newException(OutOfRange, OutOfRange, msg, err)
 }
 
 func NewUnauthenticatedError(err error, msg string) error {
-	return newError(err, Unauthenticated, msg)
+	return newException(Unauthenticated, Unauthenticated, msg, err)
 }
 
 func NewPermissionDeniedError(err error, msg string) error {
-	return newError(err, PermissionDenied, msg)
+	return newException(PermissionDenied, PermissionDenied, msg, err)
 }
 
 func NewNotFoundError(err error, msg string) error {
-	return newError(err, NotFound, msg)
+	return newException(NotFound, NotFound, msg, err)
 }
 
 func NewAbortedError(err error, msg string) error {
-	return newError(err, Aborted, msg)
+	return newException(Aborted, Aborted, msg, err)
 }
 
 func NewAlreadyExistsError(err error, msg string) error {
-	return newError(err, AlreadyExists, msg)
+	return newException(AlreadyExists, AlreadyExists, msg, err)
 }
 
 func NewResourceExhaustedError(err error, msg string) error {
-	return newError(err, ResourceExhausted, msg)
+	return newException(ResourceExhausted, ResourceExhausted, msg, err)
 }
 
 func NewCancelledError(err error, msg string) error {
-	return newError(err, Cancelled, msg)
+	return newException(Cancelled, Cancelled, msg, err)
 }
 
 func NewDataLossError(err error, msg string) error {
-	return newError(err, DataLoss, msg)
+	return newException(DataLoss, DataLoss, msg, err)
 }
 
 func NewUnknownError(err error, msg string) error {
-	return newError(err, Unknown, msg)
+	return newException(Unknown, Unknown, msg, err)
 }
 
 func NewInternalError(err error, msg string) error {
-	return newError(err, Internal, msg)
+	return newException(Internal, Internal, msg, err)
 }
 
 func NewNotImplementedError(err error, msg string) error {
-	return newError(err, NotImplemented, msg)
+	return newException(NotImplemented, NotImplemented, msg, err)
 }
 
 func NewUnavailableError(err error, msg string) error {
-	return newError(err, Unavailable, msg)
+	return newException(Unavailable, Unavailable, msg, err)
 }
 
 func NewDeadlineExceededError(err error, msg string) error {
-	return newError(err, DeadlineExceeded, msg)
+	return newException(DeadlineExceeded, DeadlineExceeded, msg, err)
 }
--- apix/handler.go
+++ apix/handler.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+var Debug = true
+
 type HandlerFunc func(ctx *gin.Context) (interface{}, error)
 
 type RouterGroup struct {
@@ -18,45 +20,88 @@ func NewRouterGroup(group *gin.RouterGroup) *RouterGroup {
 }
 
 func (r *RouterGroup) GET(relativePath string, handler HandlerFunc) *RouterGroup {
-	r.gin.GET(relativePath, wrapper(handler))
+	r.gin.GET(relativePath, Wrapper(handler))
 	return r
 }
 
 func (r *RouterGroup) POST(relativePath string, handler HandlerFunc) *RouterGroup {
-	r.gin.POST(relativePath, wrapper(handler))
+	r.gin.POST(relativePath, Wrapper(handler))
 	return r
 }
 
 func (r *RouterGroup) DELETE(relativePath string, handler HandlerFunc) *RouterGroup {
-	r.gin.DELETE(relativePath, wrapper(handler))
+	r.gin.DELETE(relativePath, Wrapper(handler))
 	return r
 }
 
 func (r *RouterGroup) PATCH(relativePath string, handler HandlerFunc) *RouterGroup {
-	r.gin.PATCH(relativePath, wrapper(handler))
+	r.gin.PATCH(relativePath, Wrapper(handler))
 	return r
 }
 
 func (r *RouterGroup) PUT(relativePath string, handler HandlerFunc) *RouterGroup {
-	r.gin.PUT(relativePath, wrapper(handler))
+	r.gin.PUT(relativePath, Wrapper(handler))
 	return r
 }
 
-func wrapper(fun HandlerFunc) gin.HandlerFunc {
+type RespBody struct {
+	Succeeded bool        `json:"succeeded"`
+	RespData  interface{} `json:"resp_data"`
+	HttpCode  int         `json:"-"`
+	Code      int         `json:"code,omitempty"`
+	Msg       string      `json:"msg,omitempty"`
+	Desc      string      `json:"desc,omitempty"`
+}
+
+type ApiException interface {
+	GetHttpCode() int
+	GetCode() int
+	GetMsg() string
+	GetDesc() string
+	Error() string
+}
+
+func Wrapper(fun HandlerFunc) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		resp, err := fun(ctx)
-		if err != nil {
-			if e, ok := err.(RespBody); ok {
-				ctx.JSON(e.Code, e)
-			} else {
-				ctx.JSON(Internal, newError(err, Internal, "服务内部出错"))
-			}
+		if err == nil {
+			ctx.JSON(http.StatusOK, RespBody{
+				Succeeded: true,
+				RespData:  resp,
+			})
 			return
 		}
 
-		ctx.JSON(http.StatusOK, RespBody{
-			Succeeded: true,
-			RespData:  resp,
+		desc := ""
+		apiException, ok := err.(ApiException)
+		if !ok {
+			if Debug {
+				desc = err.Error()
+			}
+			ctx.JSON(http.StatusInternalServerError, RespBody{
+				Succeeded: false,
+				RespData:  nil,
+				Code:      http.StatusInternalServerError,
+				Msg:       "Unknown Error",
+				Desc:      desc,
+			})
+			return
+		}
+
+		httpCode := apiException.GetHttpCode()
+		if apiException.GetHttpCode() < 100 || apiException.GetHttpCode() > 505 {
+			httpCode = http.StatusInternalServerError
+		}
+		if Debug {
+			desc = apiException.GetDesc()
+		}
+
+		ctx.JSON(httpCode, RespBody{
+			Succeeded: false,
+			RespData:  nil,
+			Code:      apiException.GetCode(),
+			Msg:       apiException.GetMsg(),
+			Desc:      desc,
 		})
 		return
 	}
