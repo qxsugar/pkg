50b12d598033015b274dae9fb1e564537ca0b5fc
sugar <qxsugar@gmail.com>
2023-11-09 18:08:35 +0800

    fix typo

--- apix/resp.go
+++ apix/resp.go
@@ -1,11 +1,11 @@
 package apix
 
 type RespBody struct {
-	Succeeded bool        `json:"succeeded"`
-	RespData  interface{} `json:"resp_data"`
-	Code      int         `json:"code,omitempty"`
-	Info      string      `json:"info,omitempty"`
-	Desc      string      `json:"desc,omitempty"`
+	Succeeded bool        `json:"succeeded"`      // 是否成功
+	RespData  interface{} `json:"resp_data"`      // 返回数据
+	Code      int         `json:"code,omitempty"` // 业务状态码
+	Info      string      `json:"info,omitempty"` // 业务提示
+	Desc      string      `json:"desc,omitempty"` // 异常提示，一般只出现在开发模式
 }
 
 type PageBody struct {
