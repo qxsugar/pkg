7a8a8488c3ebd278943eaf7daf007c89ba3a6268
qxsugar <qxsugar@gmail.com>
2025-08-07 17:03:40 +0800

    fix: fix handler

--- kit/handler.go
+++ kit/handler.go
@@ -47,40 +47,44 @@ func (r *RouterGroup) PUT(relativePath string, handler HandlerFunc) *RouterGroup
 	return r
 }
 
+// TranslateFunc 将 HandlerFunc 转换为 gin.HandlerFunc
+// 并处理错误，返回统一的响应格式
+// 如果发生错误，返回 RespBody 中的 Succeeded 为 false，并包含错误信息
+// 如果成功，返回 RespBody 中的 Succeeded 为 true，并包含响应数据
+// 如果发生业务错误，返回 RespBody 中的 Code 和 Info 字段
+// 如果发生非业务错误，返回 RespBody 中的 Code 为 InternalErrorCode，并包含错误信息
+// 如果在开发模式下，返回 RespBody 中的 Desc 字段包含详细错误描述
 func TranslateFunc(fun HandlerFunc) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var (
-			logger = zap.S().Named("TranslateFunc")
-		)
+		logger := zap.S().Named("TranslateFunc")
 
 		resp, err := fun(ctx)
-		if err == nil {
-			ctx.JSON(http.StatusOK, RespBody{Succeeded: true, RespData: resp})
+		if err != nil {
+			respBody := RespBody{
+				Succeeded: false,
+			}
+
+			switch ex := err.(type) {
+			case BusinessError:
+				respBody.Code = ex.Code()
+				respBody.Info = ex.Info()
+				if gin.IsDebugging() {
+					respBody.Desc = ex.Desc()
+				}
+			default:
+				respBody.Code = InternalErrorCode
+				respBody.Info = messages[ErrInternal]
+				if gin.IsDebugging() && err != nil {
+					respBody.Desc = err.Error()
+				}
+			}
+
+			logger.Debugf("failed to handler http, code: %d, info: %s, desc: %s", respBody.Code, respBody.Info, respBody.Desc)
+			ctx.JSON(http.StatusOK, respBody)
 			return
 		}
 
-		respBody := RespBody{
-			Succeeded: false,
-		}
-
-		switch ex := err.(type) {
-		case BusinessError:
-			respBody.Code = ex.Code()
-			respBody.Info = ex.Info()
-			if gin.IsDebugging() {
-				respBody.Desc = ex.Desc()
-			}
-		default:
-			respBody.Code = InternalErrorCode
-			respBody.Info = messages[ErrInternal]
-			if gin.IsDebugging() && err != nil {
-				respBody.Desc = err.Error()
-			}
-		}
-
-		logger.Infof("failed to handler http, code: %d, info: %s, desc: %s", respBody.Code, respBody.Info, respBody.Desc)
-
-		ctx.JSON(http.StatusOK, respBody)
+		ctx.JSON(http.StatusOK, RespBody{Succeeded: true, RespData: resp})
 		return
 	}
 }
